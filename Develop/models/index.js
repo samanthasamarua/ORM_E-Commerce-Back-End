f88deaf76537c7faf2ecfac6dd3f88895d24212e
Samantha Samarua <samantha.samarua@gmail.com>
2024-03-14 22:55:19 +0930

    06 Updated model js files - Index

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,37 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsto(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'Product_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+    through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'Product_Tags'
+});
 
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, {
+  through: {
+  model: ProductTag,
+  unique: false
+},
+// Define an alias for when data is retrieved
+as: 'Tag_Product'
+});
+
 
 module.exports = {
   Product,
