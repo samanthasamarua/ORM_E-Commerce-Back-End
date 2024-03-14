7bc0ed4b849137a6bd86b463028e162ac06d31e9
Samantha Samarua <samantha.samarua@gmail.com>
2024-03-14 22:32:30 +0930

    02 Updated model js files - Category

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,15 +1,18 @@
+const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const Sequelize = require('sequelize');
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306 
+  }
+);
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+module.exports = sequelize;
+
 
 module.exports = sequelize;
